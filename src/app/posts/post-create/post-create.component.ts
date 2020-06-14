1ed21ea95737b28a85a06cbd81f58b9cd232dd5c
wsoccorsi <wsoccorsi@gmail.com>
2020-06-13 21:39:43 -0400

    added my own comp

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+}) //markets this as a component
+
+export class PostCreateComponent { }
